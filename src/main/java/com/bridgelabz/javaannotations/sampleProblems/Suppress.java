4796277a9d5a89e5bdd4afaf464dae652223dc46
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-04-06 22:25:36 +0530

    [Nikhil] Add: Suppressing warning using annotation.

---
+++ src/main/java/com/bridgelabz/javaannotations/sampleProblems/Suppress.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.javaannotations.sampleProblems;
+
+import java.util.ArrayList;
+
+public class Suppress {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("Hello");
+        list.add(100);
+        for(Object obj : list) {
+            System.out.println(obj);
+        }
+    }
+}
